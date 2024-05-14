47ef12e7a0d2e2ea9dbe0b7234ca93a5fe0831f7
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2024-05-15 05:44:25 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application } from 'express';
+import httpStatus from 'http-status';
+import globalExceptionHandler from './app/middlewares/globalExceptionHandler';
+import routes from './app/routes';
+
+const app: Application = express();
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1', routes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(globalExceptionHandler);
+
+app.use((req, res, next) => {
+  console.log(req, res);
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'API not found',
+    errorMessages: [
+      {
+        path: '',
+        message: 'API not found'
+      }
+    ]
+  });
+});
+
+export default app;
---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { AuthenticationController } from './auth.controller';
+import { AuthValidation } from './auth.validation';
+
+const router = express.Router();
+
+router.post(
+  '/login',
+  validateRequest(AuthValidation.loginZodSchema),
+  AuthenticationController.loginUser
+);
+router.post(
+  '/refresh-token',
+  validateRequest(AuthValidation.refreshTokenZodSchema),
+  AuthenticationController.refreshToken
+);
+
+router.post('/forgot-password', AuthenticationController.forgotPassword);
+router.post('/reset-password', AuthenticationController.resetPassword);
+
+router.post(
+  '/change-password',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.STUDENT, ENUM_USER_ROLE.FACULTY),
+  validateRequest(AuthValidation.changePasswordZodSchema),
+  AuthenticationController.changePassword
+);
+
+export const authRoutes = router;
